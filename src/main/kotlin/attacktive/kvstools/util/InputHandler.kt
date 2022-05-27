bff4d9a78153b710f19ec849b0ed01c2f935ffb7
Attacktive <welcome.hell.dude@gmail.com>
2022-05-27 12:16:19 +0900

    Fix missing return statement

--- src/main/kotlin/attacktive/kvstools/util/InputHandler.kt
+++ src/main/kotlin/attacktive/kvstools/util/InputHandler.kt
@@ -3,7 +3,7 @@ package attacktive.kvstools.util
 import kotlin.system.exitProcess
 
 object InputHandler {
-	fun handle(vararg args: String, errorMessage: String) {
+	fun handle(vararg args: String, errorMessage: String): String {
 		if (args.isEmpty()) {
 			System.err.println(errorMessage)
 			exitProcess(1)
@@ -13,5 +13,7 @@ object InputHandler {
 		if (args.size > 1) {
 			println("You provided more than one argument; other than \"$argument\" will be ignored.")
 		}
+
+		return argument
 	}
 }
